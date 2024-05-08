e744110c047ba669fd386782db0fec6c1301e950
kaushik anand <anandkaushik1424@gmail.com>
2024-05-09 04:49:52 +0530

    first commit

--- components/sub/ HeroContent.tsx
+++ components/sub/ HeroContent.tsx
@@ -33,7 +33,7 @@ const HeroContent = () => {
       className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
     >
       <span>
-        <span style={{ color: "violet" }}>Namaste,</span> I'm Kaushik
+        <span style={{ color: "violet" }}>Namaste,</span>  I&apos;m Kaushik
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
           {" "}
           Anand {" "}
